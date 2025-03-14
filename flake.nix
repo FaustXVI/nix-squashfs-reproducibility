2092ba936196acafe15429d514f8bea5aa8b307d
Xavier Detant <1016863+FaustXVI@users.noreply.github.com>
2025-03-14 16:32:52 +0100

    Fix nixpkgs version

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Hashicorp vault server";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/3f0a8ac25fb674611b98089ca3a5dd6480175751";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -16,6 +16,7 @@
           let
             script = pkgs.writeShellScriptBin "create-non-reproducible-squashfs"
               ''
+                set -x
                 FILE=$(mktemp -u -t non-reproducible-XXX.squashfs)
                 # options for squashfs are copied from https://github.com/NixOS/nixpkgs/blob/845dc1e9cbc2e48640b8968af58b4a19db67aa8f/nixos/lib/make-squashfs.nix#L52
                 ${pkgs.squashfsTools}/bin/mksquashfs ${pkgs.aws-c-common} $FILE -no-hardlinks -keep-as-directory -all-root -b 1048576 -comp gzip -Xcompression-level 1 -processors 1 -root-mode 0755 -no-compression > /dev/null
