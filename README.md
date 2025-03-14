91eb08046418f1a10aa93bd6dc675e468a3ac28c
Xavier Detant <1016863+FaustXVI@users.noreply.github.com>
2025-03-14 15:39:15 +0100

    Focus on squashfs

--- README.md
+++ README.md
@@ -2,21 +2,15 @@
 
 To reproduce, clone this repository and run :
 ```
-nix build .#iso
-```
-
-Then compute the `sha256sum` of the result with :
-```
-sha256sum result/iso/nixos.iso
+nix develop -i --command bash -c "nix run"
 ```
 
 On my NixOS, I found :
 ```
-9c09867b3e073243aa8020630ae488b639f289b17dc7d68c859a8c160ba1b16b
+1960306f3d10bf282b6fc823098ff42e3633a24ca54d3bc4f27eb55c1e852156
 ```
 
 On my xubuntu, I found :
 ```
-05355dc48302cfdec8f8eb70ceadbc5edf2042724208b481463611ac280a72d1
 ```
 
--- flake.nix
+++ flake.nix
@@ -3,16 +3,32 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
-    {
-      iso = (nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          "${nixpkgs}/nixos/modules/installer/cd-dvd/iso-image.nix"
-          { system.stateVersion = "24.11"; }
-        ];
-      }).config.system.build.isoImage;
-    };
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    (flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        apps.default =
+          let
+            script = pkgs.writeShellScriptBin "create-non-reproducible-squashfs"
+              ''
+                FILE=$(mktemp -u -t non-reproducible-XXX.squashfs)
+                # options for squashfs are copied from https://github.com/NixOS/nixpkgs/blob/845dc1e9cbc2e48640b8968af58b4a19db67aa8f/nixos/lib/make-squashfs.nix#L52
+                ${pkgs.squashfsTools}/bin/mksquashfs ${pkgs.aws-c-common} $FILE -no-hardlinks -keep-as-directory -all-root -no-compression -b 1048576 -root-mode 0755 > /dev/null
+                ${pkgs.coreutils}/bin/sha256sum $FILE
+              '';
+          in
+          {
+            type = "app";
+            # options for squashfs are copied from https://github.com/NixOS/nixpkgs/blob/845dc1e9cbc2e48640b8968af58b4a19db67aa8f/nixos/lib/make-squashfs.nix#L52
+            program = "${pkgs.lib.getExe script}";
+          };
+          devShells.default = pkgs.mkShell {
+            packages = [ pkgs.nix ];
+          };
+      }));
 }
