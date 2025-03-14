3a72a5ca8e72a8092637a486d6f8193a9bf5fc4b
Xavier Detant <1016863+FaustXVI@users.noreply.github.com>
2025-03-14 17:33:37 +0100

    Remove aws ref

--- flake.nix
+++ flake.nix
@@ -3,15 +3,13 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    nixpkgs-aws.url = "github:NixOS/nixpkgs/3f0a8ac25fb674611b98089ca3a5dd6480175751";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, nixpkgs-aws, ... }@inputs:
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
     (flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        pkgs-aws = import nixpkgs-aws { inherit system; };
       in
       {
         apps.default =
@@ -21,7 +19,7 @@
                 set -x
                 FILE=$(mktemp -u -t non-reproducible-XXX.squashfs)
                 # options for squashfs are copied from https://github.com/NixOS/nixpkgs/blob/845dc1e9cbc2e48640b8968af58b4a19db67aa8f/nixos/lib/make-squashfs.nix#L52
-                ${pkgs.squashfsTools}/bin/mksquashfs ${pkgs-aws.aws-c-common} $FILE -no-hardlinks -keep-as-directory -all-root -b 1048576 -comp gzip -Xcompression-level 1 -processors 1 -root-mode 0755 -no-compression > /dev/null
+                ${pkgs.squashfsTools}/bin/mksquashfs myaws $FILE -no-hardlinks -keep-as-directory -all-root -b 1048576 -comp gzip -Xcompression-level 1 -processors 1 -root-mode 0755 -no-compression > /dev/null
                 ${pkgs.coreutils}/bin/sha256sum $FILE
               '';
           in
