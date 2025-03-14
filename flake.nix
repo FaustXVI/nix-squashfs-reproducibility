3f351649361ff0c10d680df5cf0601f56c8c77e5
Xavier Detant <1016863+FaustXVI@users.noreply.github.com>
2025-03-14 16:43:46 +0100

    Fix aws version

--- flake.nix
+++ flake.nix
@@ -2,14 +2,16 @@
   description = "Hashicorp vault server";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/3f0a8ac25fb674611b98089ca3a5dd6480175751";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs-aws.url = "github:NixOS/nixpkgs/3f0a8ac25fb674611b98089ca3a5dd6480175751";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+  outputs = { self, nixpkgs, flake-utils, nixpkgs-aws, ... }@inputs:
     (flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        pkgs-aws = import nixpkgs-aws { inherit system; };
       in
       {
         apps.default =
@@ -19,7 +21,7 @@
                 set -x
                 FILE=$(mktemp -u -t non-reproducible-XXX.squashfs)
                 # options for squashfs are copied from https://github.com/NixOS/nixpkgs/blob/845dc1e9cbc2e48640b8968af58b4a19db67aa8f/nixos/lib/make-squashfs.nix#L52
-                ${pkgs.squashfsTools}/bin/mksquashfs ${pkgs.aws-c-common} $FILE -no-hardlinks -keep-as-directory -all-root -b 1048576 -comp gzip -Xcompression-level 1 -processors 1 -root-mode 0755 -no-compression > /dev/null
+                ${pkgs.squashfsTools}/bin/mksquashfs ${pkgs-aws.aws-c-common} $FILE -no-hardlinks -keep-as-directory -all-root -b 1048576 -comp gzip -Xcompression-level 1 -processors 1 -root-mode 0755 -no-compression > /dev/null
                 ${pkgs.coreutils}/bin/sha256sum $FILE
               '';
           in
