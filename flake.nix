7e3146be345ccafae4fe1c027d9a6f629bd34d6e
Xavier Detant <1016863+FaustXVI@users.noreply.github.com>
2025-03-14 16:15:55 +0100

    Add more options

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
               ''
                 FILE=$(mktemp -u -t non-reproducible-XXX.squashfs)
                 # options for squashfs are copied from https://github.com/NixOS/nixpkgs/blob/845dc1e9cbc2e48640b8968af58b4a19db67aa8f/nixos/lib/make-squashfs.nix#L52
-                ${pkgs.squashfsTools}/bin/mksquashfs ${pkgs.aws-c-common} $FILE -no-hardlinks -keep-as-directory -all-root -no-compression -b 1048576 -root-mode 0755 > /dev/null
+                ${pkgs.squashfsTools}/bin/mksquashfs ${pkgs.aws-c-common} $FILE -no-hardlinks -keep-as-directory -all-root -b 1048576 -comp gzip -Xcompression-level 1 -processors 1 -root-mode 0755 -no-compression > /dev/null
                 ${pkgs.coreutils}/bin/sha256sum $FILE
               '';
           in
